dcb27bb183b3229ecd33f5d3743e3a857716efd2
aigubeev <87580669+aigubeev@users.noreply.github.com>
2022-10-08 14:17:22 +0300

    Create main.tf

---
+++ 07-terraform-02-syntax/terraform/main.tf
@@ -0,0 +1,77 @@
+#Variables
+variable "yandex_cloud_id" {
+  default = "b1g9ilrcqm2noqtvd2om"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gdit6jmusqo5af5j40"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd88d14a6790do254kj7"
+}
+
+#Make provider
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version =  ">= 0.13"
+}
+
+provider yandex {
+  token     = "my_token"
+  cloud_id  = "${var.yandex_cloud_id}"
+  folder_id = "${var.yandex_folder_id}"
+}
+
+
+#Make compute_instance
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-node01"
+      type        = "network-ssd"
+      size        = "50"
+    }
+  }
+
+ network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+#Make network
+
+resource "yandex_vpc_network" "default" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "default" {
+  name = "subnet"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.default.id}"
+  v4_cidr_blocks = ["192.168.101.0/24"]
+}
